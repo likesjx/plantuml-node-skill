bc18fed138f8115599c3d291913501a28e6b66d2
likesjx <likesjx@gmail.com>
2026-02-11 15:06:45 -0500

    bootstrapped plantuml-node-skill

---
+++ index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// Simple wrapper using @lumino/plantuml or plantuml-core (plantuml-core is wasm/js port)
+// This is a placeholder; install and use a PlantUML JS renderer here.
+console.log('plantuml-node-skill placeholder');
